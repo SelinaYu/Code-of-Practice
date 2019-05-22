b349f2124d47f3574588e845f7698fee0f9021cd
licyd <licyd@yonyou.com>
2019-05-22 18:01:56 +0800

    Updates

---
+++ precache-manifest.863d74789954531dd0de513627311e89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bdf8f3d1387d49f285994bb40e5c90",
+    "url": "/Code-of-Practice/dragscaleview/index.html"
+  },
+  {
+    "revision": "a1d0a7461f6ee2cfe8e5",
+    "url": "/Code-of-Practice/dragscaleview/static/js/2.b1e48e36.chunk.js"
+  },
+  {
+    "revision": "cf5ad9c1950f9239963c",
+    "url": "/Code-of-Practice/dragscaleview/static/js/main.e793c375.chunk.js"
+  },
+  {
+    "revision": "9b3a0eec83a0d672a24d",
+    "url": "/Code-of-Practice/dragscaleview/static/js/runtime~main.ba3eb466.js"
+  },
+  {
+    "revision": "ec0793f5c9d240ae9bf0f9d80fdcc7c6",
+    "url": "/Code-of-Practice/dragscaleview/static/media/style.ec0793f5.less"
+  }
+]);
